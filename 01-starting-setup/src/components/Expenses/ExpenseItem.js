509abdefad739b56540931da78c4e93c2ce32e81
lunazzao <zhao.jiayu@northeastern.edu>
2023-06-02 11:24:29 -0400

    Initial commit

---
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+
+  // const expenseDate = new Date(2023, 5,28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+  return (
+    <Card className="expense-item">
+        <ExpenseDate date = {props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button> Change Title </button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
